b10f1f44ae97e496e6164945ce3f21f2ae60b371
yoursaas <91716615+yoursaas@users.noreply.github.com>
2022-12-07 23:42:40 +0200

    Update and rename wergwegr.txt to task1(to_bigger).cpp

---
+++ 31.10/task1(to_bigger).cpp
@@ -0,0 +1,26 @@
+#include <iostream> 
+#include <algorithm>
+ 
+int main()
+{
+	const int length = 12;
+	int array[length] = { 30, 50, 20, 10, 40, 98, 32, 67, 21, 918, 11, 32 };
+ 
+	for (int startIndex = 0; startIndex < length - 1; ++startIndex)
+	{
+		int smallestIndex = startIndex;
+ 
+		for (int currentIndex = startIndex + 1; currentIndex < length; ++currentIndex)
+		{
+			if (array[currentIndex] < array[smallestIndex])
+				smallestIndex = currentIndex;
+		}
+ 
+		std::swap(array[startIndex], array[smallestIndex]);
+	}
+ 
+	for (int index = 0; index < length; ++index)
+		std::cout << array[index] << ' ';
+ 
+	return 0;
+}
