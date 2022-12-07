b851e458ebb22dc633a370ed9bc4c8da35fbca33
yoursaas <91716615+yoursaas@users.noreply.github.com>
2022-12-07 23:39:44 +0200

    Create task7.cpp

---
+++ 26.09/task7.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std; 
+int main(void){ 
+    float num1= 2.3; 
+    float num2= 2.3; 
+    float num3= 2.123456; 
+    float num4= 2.123456; 
+    float num5= 2.123456; 
+    cout << num1 <<'\n'; 
+    cout << num2 <<"0"<<'\n'; 
+    cout << num3 <<'\n'; 
+    cout << round (num4 * 100)/100 <<'\n'; 
+    cout << int (num5) <<'\n'; 
+}
